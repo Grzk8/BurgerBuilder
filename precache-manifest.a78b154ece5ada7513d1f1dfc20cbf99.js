c6cf14cedea6f20d2289a78b6f382fb6ad02c43b
Grzegorz Kaczor <grzesiek125@op.pl>
2020-10-26 18:41:33 +0100

    Updates

---
+++ precache-manifest.a78b154ece5ada7513d1f1dfc20cbf99.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "18deddde283e0fe35433195d932ea478",
+    "url": "/BurgerBuilder/index.html"
+  },
+  {
+    "revision": "21745490af0b8d1f5eff",
+    "url": "/BurgerBuilder/static/css/main.a81937ae.chunk.css"
+  },
+  {
+    "revision": "997c594bdbf26671f784",
+    "url": "/BurgerBuilder/static/js/2.ab82e210.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/BurgerBuilder/static/js/2.ab82e210.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "21745490af0b8d1f5eff",
+    "url": "/BurgerBuilder/static/js/main.67271fc4.chunk.js"
+  },
+  {
+    "revision": "ca6035b1e1cf1d46a9b9",
+    "url": "/BurgerBuilder/static/js/runtime-main.496e965d.js"
+  },
+  {
+    "revision": "b8503d262bffbfb7c67fd6762963e7d1",
+    "url": "/BurgerBuilder/static/media/burger-logo.b8503d26.png"
+  }
+]);
--- src/components/Layout/Button/Button.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const button = (props) => (
-<button 
-onClick={props.clicked}
-className={[props.btnType, 'Button'].join(' ')} >{props.children}</button>
-);
-
-export default button;
