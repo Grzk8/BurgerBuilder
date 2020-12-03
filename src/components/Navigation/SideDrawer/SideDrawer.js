edef4bff1906f3a142582719084600d55fff56f9
Grzegorz Kaczor <grzesiek125@op.pl>
2020-12-03 19:01:55 +0100

    ipmrv

--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -14,7 +14,7 @@ const sideDrawer = (props) => {
     return(
         <>
             <Backdrop show={props.open} clicked={props.closed}/>
-            <div className={attachedClasses.join(' ')}>
+            <div className={attachedClasses.join(' ')} onClick={props.closed}>
                 <Logo height='11%'/>
                 <nav>
                     <NavigationItems isAuthenticated={props.isAuth}/>
--- src/store/actions/order.js
+++ src/store/actions/order.js
@@ -27,7 +27,6 @@ export const purchaseBurger = ( orderData, token ) => {
         dispatch( purchaseBurgerStart() );
         axios.post( 'https://burgerbuilder-166a2.firebaseio.com/orders.json?auth=' + token, orderData )
             .then( response => {
-                console.log( response.data );
                 dispatch( purchaseBurgerSuccess( response.data.name, orderData ) );
             } )
             .catch( error => {
